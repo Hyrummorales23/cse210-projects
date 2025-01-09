7d07e2cd466a82757daf87a6cefb945df03dbb7b
Hyrummorales23 <hyrummorales23@gmail.com>
2025-01-09 14:38:22 -0600

    Finishes Excercise 3 Week 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to the Guess My Number game!");
+        Console.WriteLine("Would you like to enter the magic number yourself (type '1') or let the computer generate it (type '2')?");
+
+        // Ask the user if they want to enter the magic number or generate a random one
+        int choice = int.Parse(Console.ReadLine());
+        int magicNumber;
+
+        if (choice == 1)
+        {
+            // User provides the magic number
+            Console.Write("Enter the magic number: ");
+            magicNumber = int.Parse(Console.ReadLine());
+        }
+        else
+        {
+            // Generate a random magic number
+            Random randomGenerator = new Random();
+            magicNumber = randomGenerator.Next(1, 101); // Random number between 1 and 100
+            Console.WriteLine("The computer has generated a random magic number. Try to guess it!");
+        }
+
+        int guess = -1; // Initialize with a value different from the magic number
+
+        // Loop until the guess matches the magic number
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! Congratulations!");
+            }
+        }
     }
 }
