4aaa09476d648ca2bab94714cd28d16d3e5b94f6
Hyrummorales23 <hyrummorales23@gmail.com>
2025-01-09 11:44:26 -0600

    Finished Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Prompting the user to enter its first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompting the user to enter its last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Displaying the name as James Bond Style
+        Console.WriteLine($"Yor name is {lastName}, {firstName} {lastName}. ");
     }
 }
