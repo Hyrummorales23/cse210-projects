623c54cfda7db0dfc32ca786ebe5872b29ee4da2
Hyrummorales23 <hyrummorales23@gmail.com>
2025-01-09 14:12:05 -0600

    Finished excersice 2 week 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,78 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        // Determine letter
+
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine sign
+        int lastDigit = grade % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit >= 3)
+        {
+            sign = "";
+        }
+        else
+        {
+            sign = "-";
+        }
+
+        //Exception on the sign for the letter grade
+        if (letter == "A" && lastDigit <= 2)
+        {
+            sign = "-";  // No A+ grade
+        }
+        else if (letter == "A" && lastDigit > 2)
+        {
+            sign = "";
+        }
+        else if (letter == "F")
+        {
+            sign = "";  // No F+ or F- grade
+        }
+
+        // Letter grade with sign display
+        Console.WriteLine($"Your letter grade is: {letter}{sign}.");
+
+        // Determine Pass/Fail and display message
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Keep Trying! You can do better next time.");
+        }
+
     }
 }
