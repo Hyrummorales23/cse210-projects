c68970a632acd871329ea8e4ce25008b8d19c9ab
Hyrummorales23 <hyrummorales23@gmail.com>
2025-02-13 17:06:29 -0600

    week 7

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime Date => _date;
+    public int Minutes => _minutes;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {GetType().Name} ({_minutes} min) - Distance: {GetDistance():F2} miles, Speed: {GetSpeed():F2} mph, Pace: {GetPace():F2} min per mile";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2025, 02, 10), 30, 3.0),
+            new Cycling(new DateTime(2025, 02, 11), 45, 15.0),
+            new Swimming(new DateTime(2025, 02, 12), 60, 40),
+            new Running(new DateTime(2025, 02, 13), 25, 4.0)
+        };
+        Console.WriteLine();
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+        Console.WriteLine();
     }
 }
